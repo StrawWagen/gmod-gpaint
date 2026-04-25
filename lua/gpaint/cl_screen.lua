f077752ef21554deb40500da4e8e6218b108ab88
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-04-25 02:16:51 -0700

    changes

--- lua/gpaint/cl_screen.lua
+++ lua/gpaint/cl_screen.lua
@@ -615,6 +615,14 @@ local blockBinds = {
     ["+reload"] = true
 }
 
+local exceptionWeapons = {
+    ["gmod_camera"] = true -- They're trying to take a photo of the screen, let them!
+}
+
 hook.Add( "PlayerBindPress", "GPaint.BlockBindsWhenFocused", function( _, bind )
-    if blockBinds[bind] and focusedId then return true end
+    if blockBinds[bind] and focusedId then -- Blockable bind, and we're focusing on a screen
+        local weapon = LocalPlayer():GetActiveWeapon()
+        if not IsValid( weapon ) then return true end
+        if not exceptionWeapons[weapon:GetClass()] then return true end
+    end
 end )
